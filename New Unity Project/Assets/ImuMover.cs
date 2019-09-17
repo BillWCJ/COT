a2c00409a02ad87b01b573e2d0cf156131498207
BillWenChaoJiang <jiang.bill@hotmail.com>
2019-09-17 04:09:18 -0700

    feat(imu): integrated IMU for head tracking

---
+++ New Unity Project/Assets/ImuMover.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets
+{
+    class ImuMover : MonoBehaviour
+    {
+        SerialPort _serialPort = new SerialPort("COM3", 115200);
+        float x = 0;
+        float y = 0;
+        float z = 0;
+
+        public ImuMover()
+        {
+
+        }
+        void Awake()
+        {
+            Debug.Log("Awake");
+        }
+
+        IEnumerator Start()
+        {
+            Debug.Log("Start1");
+            if (!_serialPort.IsOpen)
+            {
+                _serialPort.Open();
+            }
+            yield return new WaitForSeconds(2.5f);
+            Debug.Log("Start2");
+        }
+        
+        void Update()
+        {
+            String line = _serialPort.ReadLine();
+            Debug.Log("line" + line);
+
+            String[] split = line.Split();
+            if (split.Length == 4 && split[3] == "Done")
+            {
+                x = float.Parse(split[0]);
+                x = x < 0 ? x + 360 : x;
+                y = float.Parse(split[1]);
+                y = y < 0 ? y + 360 : y;
+                z = float.Parse(split[2]);
+                z = z < 0 ? z + 360 : z;
+
+                Debug.Log($"{x} {y} {z}");
+                _serialPort.BaseStream.Flush();
+            }
+            Debug.Log($"Set {x} {y} {z}");
+            transform.rotation = Quaternion.Euler(z, x, y);
+        }
+    }
+}
---
+++ TestConsoleProject/Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO.Ports;
+using System.Threading;
+namespace TestConsoleProject
+{
+    class Program
+    {
+        static SerialPort _serialPort;
+        static void Main(string[] args)
+        {
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM3";//Set your board COM
+            _serialPort.BaudRate = 115200;
+            _serialPort.Open();
+            _serialPort.DataReceived += _serialPort_DataReceived;
+            while (true)
+            {
+                Thread.Sleep(200);
+            }
+        }
+
+        public class ImuData
+        {
+            public double x { get; set; }
+            public double y { get; set; }
+            public double z { get; set; }
+
+            public override string ToString()
+            {
+                return String.Format($"{x} {y} {z}");
+            }
+        }
+
+        private static void _serialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort serialPort = sender as SerialPort;
+            if (serialPort == null)
+            {
+                Console.WriteLine("Failed to Cast");
+            }
+            else
+            {
+
+                string line = serialPort.ReadLine();
+                Console.WriteLine("line" + line);
+
+                try {
+                    ImuData data = JsonConvert.DeserializeObject<ImuData>(line);
+                    Console.WriteLine("data" + data);
+                }
+                catch(Exception ex){
+                    Console.WriteLine(ex);
+                }
+
+
+
+            }
+
+        }
+    }
+}
